3f97f332fdb917f11a825786c4f4bfbfd2a3169a
moonkekigrl <sophreuslun@gmail.com>
2024-08-02 12:06:25 +0300

    third iteration

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,27 @@ const container = document.querySelector('.content');
 const cardsContainer = container.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
+function deleteCard(cardItem) {
+    cardItem.remove();
+}
+
 function createCard (cardValue, deleteCardEvent) {
     const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
-    const cardImage = cardItem.querySelector('.card__image').src = cardValue.link;
-    cardItem.querySelector('.card__image').alt = cardValue.name;
+    const cardImage = cardItem.querySelector('.card__image');
+    cardImage.src = cardValue.link;
+    cardImage.alt = cardValue.name;
     cardItem.querySelector('.card__title').textContent = cardValue.name;
     
-
-    const deleteButton = cardItem.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', () => {
-     cardItem.remove(deleteCardEvent);
-     });
+    cardItem.querySelector('.card__delete-button').addEventListener('click', () => {
+        deleteCard(cardItem);
+    });
     
-return cardItem;
+    return cardItem;
 };
 
 function renderCards() {
-    initialCards.forEach(function(cardValue) {
-        cardsContainer.append(createCard(cardValue));
+    initialCards.forEach(function(cardValue, deleteCard) {
+        cardsContainer.append(createCard(cardValue, deleteCard));
     });
 };
 renderCards();
